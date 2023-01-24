c1bb332edda8e02d4432c0d88f9ec4dd3389e354
AchmadRaihan <91414460+AchmadRaihan@users.noreply.github.com>
2023-01-24 13:42:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ from plotly import graph_objs as go
 #---------------------------------#
 # Page layout
 # Page expands to full width
-st.set_page_config(page_title='Water Level App',
-    layout='wide')
+st.set_page_config(page_title='Sistem PTAS', layout='wide')
 
 
 #---------------------------------#
@@ -23,7 +22,7 @@ def build_model(df):
 	future = m.make_future_dataframe(periods=period, freq='h')
 	forecast = m.predict(future)
     # Plot forecast
-	st.write(f'Forecast plot for {n_days} days')
+	st.write(f'Grafik prediksi untuk {n_days} hari')
 	fig1 = plot_plotly(m, forecast, xlabel='Waktu', ylabel='Tinggi Air (m)')
 	st.plotly_chart(fig1)
 
@@ -31,58 +30,39 @@ def plot_raw_data(df):
     df['ds'] = pd.to_datetime(df['ds'])
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=df['ds'], y=df['y'], name="stock_open"))
-    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+    fig.layout.update(title_text='Geser untuk mengatur penglihatan data', xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
 
 #---------------------------------#
 # Sidebar - Collects user input features into dataframe
-with st.sidebar.header('Upload your CSV data'):
-    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+with st.sidebar.header('*Upload .csv file*'):
+    uploaded_file = st.sidebar.file_uploader(type=["csv"])
     st.sidebar.markdown("""
-	[Example CSV input file](https://github.com/AchmadRaihan/kalu-data/blob/main/kalu.csv)
+	[Contoh .csv *file*](https://github.com/AchmadRaihan/kalu-data/blob/main/kalu.csv)
 	""")
 # Sidebar - Days of prediction
-n_days = st.sidebar.slider('Days of prediction:', 0, 7)
-period = (n_days * 24) + 15
+n_days = st.sidebar.slider('Durasi Hari Prediksi:', 0, 7)
+period = (n_days * 24)
 
 
 #---------------------------------#
 # Main panel
 # Displays the dataset
 st.write("""
-# Simple Water Level Prediction App
-This app predicts the **Water Level**.
+# Sistem Prediksi Tinggi Air Sungai
+*Dashboard* hasil prediksi tinggi air sungai.
 """)
 st.subheader('Dataset')
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    st.markdown('**Glimpse of dataset**')
+    st.markdown('**Hasil Prediksi Tinggi Air Sungai**')
     plot_raw_data(df)
     build_model(df)
 else:
-    st.info('Awaiting for CSV file to be uploaded.')
-    if st.button('Press to use Example Dataset'):
+    st.info('*Upload file* .csv untuk melihat hasil prediksi tinggi air sungai.')
+    if st.button('Gunakan contoh *data set*'):
         df = pd.read_csv('kalu.csv')
         plot_raw_data(df)
         build_model(df)
-        # Diabetes dataset
-        #diabetes = load_diabetes()
-        #X = pd.DataFrame(diabetes.data, columns=diabetes.feature_names)
-        #Y = pd.Series(diabetes.target, name='response')
-        #df = pd.concat( [X,Y], axis=1 )
-
-        #st.markdown('The Diabetes dataset is used as the example.')
-        #st.write(df.head(5))
-
-        # Boston housing dataset
-        #boston = load_boston()
-        #X = pd.DataFrame(boston.data, columns=boston.feature_names)
-        #Y = pd.Series(boston.target, name='response')
-        #df = pd.read_csv('kalu.csv')
-
-        #st.markdown('The Boston housing dataset is used as the example.')
-        #st.write(df.head(5))
-
-        #build_model(df)
